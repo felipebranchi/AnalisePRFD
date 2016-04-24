dc50fdd1ecd021bfc293a015bcf6893d313106a0
Renata <rcgivisiez@gmail.com>
2016-04-24 00:21:43 -0300

    Inserção dos campos nas páginas de remoção de lixo.

--- views/solicitacao-lixo-comum.php
+++ views/solicitacao-lixo-comum.php
@@ -14,6 +14,62 @@ if ( $sessionClass->usuarioLogado() === false ) {
 include_once("partials/head.php");
 include_once("partials/navbar.php"); ?>
 
+<div class="container">
+    <div class="col-lg-8">
+        <div id="titleposition" class="page-header">
+            <h1>Solicitação de Remoção de Lixo Comum</h1>
+        </div>
+
+        <div class="container col-lg-8">
+            <form method="POST" action="" enctype="multipart/form-data">
+                <h3>Insira o endereço onde o lixo se encontra.</h3>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="cep">CEP</label>
+                    <input type="text" class="form-control" id="cep" placeholder="CEP">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="uf">UF</label>
+                    <input type="text" class="form-control" id="uf" placeholder="UF">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="cidade">Cidade</label>
+                    <input type="text" class="form-control" id="cidade" placeholder="Cidade">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="bairro">Bairro</label>
+                    <input type="text" class="form-control" id="bairro" placeholder="Bairro">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="endereco">Endereço</label>
+                    <input type="text" class="form-control" id="endereco" placeholder="Endereço">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-8 col-md-4">
+                    <label for="endereco_complemento">Complemento</label>
+                    <input type="text" class="form-control" id="endereco_complemento" placeholder="Complemento">
+                </div>
+                <br>
+                
+                <div class="form-group col-lg-10 col-md-4">
+                    <button type="submit" id="subm" class="btn btn-success">Enviar</button>
+                </div>
+            </form>
+
+
+        </div> <!-- /container -->
+    </div>
+</div>
+
 <?php include_once("partials/bottom.php"); ?>
 
 ?>
