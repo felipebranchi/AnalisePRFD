2d220b3d966ee34b389decc99eb7205f869ba847
Renata Givisiez <rcgivisiez@gmail.com>
2016-06-21 10:11:11 -0300

    Descriçao, traducoes para pt-BR

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">EcoService</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    EcoService é um sistema feito para realização de solicitações de serviços disponibilizados pela 
+                    prefeitura, geralmente. Consiste em basicamente, solicitar poda e remoção de árvores e remoção 
+                    de lixo em lugares públicos, bastando apenas passar o endereço.
                 </div>
             </div>
         </div>
